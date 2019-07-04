26a7003405bfe1d522e77c4e5774b406e386a88b
Jose A. Braojos <jabraojos@profile.es>
2019-07-04 16:30:50 +0200

    Recipes list and detail

--- Shopping/Shopping/src/app/app.component.ts
+++ Shopping/Shopping/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shopping';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
