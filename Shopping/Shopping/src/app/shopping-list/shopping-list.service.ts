108b2fdc7f614a96e50e2b77d290a7956891c518
Jose A. Braojos <jabraojos@profile.es>
2019-09-26 16:01:39 +0200

    Ingredients to shopping list.

--- Shopping/Shopping/src/app/shopping-list/shopping-list.service.ts
+++ Shopping/Shopping/src/app/shopping-list/shopping-list.service.ts
@@ -3,10 +3,7 @@ import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
     ingredientsChanged = new EventEmitter<Ingredient[]>();
-    private ingredients: Ingredient[] = [
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomatoes', 10)
-    ];
+    private ingredients: Ingredient[] = [];
 
     getIngredients() {
         return this.ingredients.slice();
@@ -15,5 +12,25 @@ export class ShoppingListService {
     addIngredient(ingredient: Ingredient){
         this.ingredients.push(ingredient);
         this.ingredientsChanged.emit(this.ingredients.slice());
+    }
+
+    addIngredients(ingredients: Ingredient[]){
+        for(let ingredient of ingredients){
+            var ingredientInList = this.ingredients.filter(
+                function(el){
+                    return el.name === ingredient.name;
+                }
+            );
+            
+            if(ingredientInList.length === 0){
+                this.ingredients.push(ingredient);    
+            } else {
+                //Añadimos el ingrediente con la nueva cantidad
+                ingredientInList[0].amount += ingredient.amount; 
+            }
+        }
+
+        this.ingredientsChanged.emit(this.ingredients.slice());
+
     }
 }
